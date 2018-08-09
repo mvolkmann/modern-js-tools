4990ec9167257a85abd243015ae362762b01a39d
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2018-08-09 12:20:39 -0500

    re-enabled a test

--- src/demo.test.js
+++ src/demo.test.js
@@ -20,7 +20,7 @@ describe('demo', () => {
     expect(event.preventDefault).toBeCalled();
   });
 
-  test.skip('handleNameChange', () => {
+  test('handleNameChange', () => {
     const nameInput = document.createElement('input');
     nameInput.value = 'Mark';
     const greetButton = document.createElement('button');
